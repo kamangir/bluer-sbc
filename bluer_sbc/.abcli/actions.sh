70e32df38392ef1daffe8820adb8655634229816
kamangir <arash@kamangir.net>
2025-04-03 18:28:46 -0700

    ++ - kamangir/bolt#746

--- bluer_sbc/.abcli/actions.sh
+++ bluer_sbc/.abcli/actions.sh
@@ -4,7 +4,7 @@ function bluer_sbc_action_git_before_push() {
     bluer_sbc build_README
     [[ $? -ne 0 ]] && return 1
 
-    [[ "$(abcli_git get_branch)" != "current" ]] &&
+    [[ "$(abcli_git get_branch)" != "main" ]] &&
         return 0
 
     bluer_sbc pypi build
