9fe1fc90edac7aafbe65125c01cbce0d91d5912f
kamangir <arash@kamangir.net>
2025-06-10 09:34:41 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/.abcli/ROS/seed.sh
+++ bluer_sbc/.abcli/ROS/seed.sh
@@ -9,4 +9,9 @@ function bluer_ai_seed_headless_ubuntu_rpi() {
     seed="${seed}sudo apt remove unattended-upgrades$delim"
     seed="${seed}sudo apt update && sudo apt upgrade$delim_section"
 
+    seed="${seed}sudo mkdir -p /etc/systemd/system/getty@tty1.service.d$delim"
+    seed="$seed$(bluer_ai_seed add_file $abcli_path_git/bluer-sbc/bluer_sbc/ROS/override.conf /etc/systemd/system/getty@tty1.service.d/override.conf)$delim"
+    seed="${seed}sudo systemctl daemon-reexec$delim"
+    seed="${seed}sudo systemctl restart getty@tty1$delim_section"
+
 }
