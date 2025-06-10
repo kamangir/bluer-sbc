387803e40b9fc41baffc8d5a0d34f341dd4a1004
kamangir <arash@kamangir.net>
2025-06-10 17:14:07 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/ROS/__main__.py
+++ bluer_sbc/ROS/__main__.py
@@ -5,6 +5,7 @@ from blueness.argparse.generic import sys_exit
 
 from bluer_sbc import NAME
 from bluer_sbc.logger import logger
+from bluer_sbc.ROS.session import start_session
 
 NAME = module.name(__file__, NAME)
 
@@ -12,10 +13,13 @@ parser = argparse.ArgumentParser(NAME)
 parser.add_argument(
     "task",
     type=str,
-    help="void",
+    help="start_session",
 )
 args = parser.parse_args()
 
-success = None
-
+success = False
+if args.task == "task":
+    success = start_session()
+else:
+    success = None
 sys_exit(logger, NAME, args.task, success)
---
+++ bluer_sbc/ROS/session.py
@@ -0,0 +1,7 @@
+from bluer_sbc.logger import logger
+
+
+def start_session() -> bool:
+    logger.info("🪄")
+
+    return True
