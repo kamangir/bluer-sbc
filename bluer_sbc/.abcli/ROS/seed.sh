cd4831fe1d60e3341978a9a49b44b272917d39bc
kamangir <arash@kamangir.net>
2025-06-10 09:46:25 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/.abcli/ROS/seed.sh
+++ bluer_sbc/.abcli/ROS/seed.sh
@@ -7,11 +7,24 @@ function bluer_ai_seed_headless_ubuntu_rpi() {
 
     seed="${seed}sudo systemctl disable unattended-upgrades$delim"
     seed="${seed}sudo apt remove unattended-upgrades$delim"
-    seed="${seed}sudo apt update && sudo apt upgrade$delim_section"
+    seed="${seed}sudo apt update$delim"
+    seed="${seed}sudo apt upgrade$delim"
+    seed="${seed}sudo apt install -y python3-venv$delim_section"
 
     seed="${seed}sudo mkdir -p /etc/systemd/system/getty@tty1.service.d$delim"
     seed="$seed$(bluer_ai_seed add_file $abcli_path_git/bluer-sbc/bluer_sbc/ROS/override.conf /etc/systemd/system/getty@tty1.service.d/override.conf)$delim"
     seed="${seed}sudo systemctl daemon-reexec$delim"
     seed="${seed}sudo systemctl restart getty@tty1$delim_section"
+
+    bluer_ai_seed add_ssh_key
+
+    bluer_ai_seed add_repo
+
+    seed="${seed}touch ~/storage/temp/ignore/headless$delim_section"
+
+    bluer_ai_seed add_bluer_ai_env
+
+    seed="${seed}pip install --upgrade pip --no-input$delim"
+    seed="${seed}pip3 install -e .$delim_section"
 
 }
