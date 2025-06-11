d61e9e2a962f11f849c17929beeea723d9be6a1d
kamangir <arash@kamangir.net>
2025-06-11 09:09:15 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/ROS/session.py
+++ bluer_sbc/ROS/session.py
@@ -1,7 +1,29 @@
+import keyboard
+import time
+
+from blueness import module
+
+from bluer_sbc import NAME
+from bluer_sbc.session.functions import reply_to_bash
 from bluer_sbc.logger import logger
+
+NAME = module.name(__file__, NAME)
+
+bash_keys = {
+    "e": "exit",
+    "r": "reboot",
+    "s": "shutdown",
+    "u": "update",
+}
 
 
 def start_session() -> bool:
-    logger.info("🪄")
+    logger.info(f"{NAME}.start_session...")
 
-    return True
+    while True:
+        for key, action in bash_keys.items():
+            if keyboard.is_pressed(key):
+                reply_to_bash(action)
+                return False
+
+        time.sleep(0.1)
