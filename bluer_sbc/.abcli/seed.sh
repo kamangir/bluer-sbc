037be3c120a17cc9b5bcc468ae63a5ce2d6a47a5
kamangir <arash@kamangir.net>
2025-07-05 11:11:50 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/.abcli/seed.sh
+++ bluer_sbc/.abcli/seed.sh
@@ -68,3 +68,17 @@ function bluer_sbc_seed() {
         seed="${seed}bluer_ai init$delim_section"
     fi
 }
+
+function bluer_ai_seed_swallow_raspbian() {
+    bluer_ai_seed add_repo repo=bluer-ugv
+    seed="${seed}pip3 install -e .$delim_section"
+
+    seed="${seed}sudo apt install -y libopenblas-dev$delim"
+
+    seed="${seed}cd$delim"
+    seed="${seed}wget https://github.com/nmilosev/pytorch-arm-builds/releases/download/v1.13.0/torch-1.13.0-cp39-cp39-linux_armv7l.whl$delim"
+    seed="${seed}pip3 install torch-1.13.0-cp39-cp39-linux_armv7l.whl$delim_section"
+
+    bluer_ai_seed add_repo repo=bluer-algo
+    seed="${seed}pip3 install -e .$delim_section"
+}
