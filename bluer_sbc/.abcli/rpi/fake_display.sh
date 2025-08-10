0f003bf33b1ec838acef24e34eec35dca16074f1
kamangir <arash@kamangir.net>
2025-08-10 11:49:04 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/.abcli/rpi/fake_display.sh
+++ bluer_sbc/.abcli/rpi/fake_display.sh
@@ -4,5 +4,10 @@ function bluer_sbc_rpi_fake_display() {
     local options=$1
     local do_dryrun=$(bluer_ai_option_int "$options" dryrun 0)
 
+    if [[ "$abcli_is_rpi" == false ]]; then
+        bluer_ai_log_warning "rpi not found."
+        return 0
+    fi
+
     :
 }
