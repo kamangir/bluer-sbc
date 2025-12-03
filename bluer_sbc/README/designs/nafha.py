a662c054ca7b0410304e9e6acd3d685d5c06c90a
kamangir <arash@kamangir.net>
2025-12-03 20:20:10 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/README/designs/nafha.py
+++ bluer_sbc/README/designs/nafha.py
@@ -2,7 +2,7 @@ from bluer_objects import README
 from bluer_objects.README.items import ImageItems
 
 from bluer_sbc.README.designs.consts import assets2
-from bluer_sbc.README.designs.pwm_generator import parts
+from bluer_sbc.README.designs import pwm_generator, battery_bus
 from bluer_sbc.README.design import design_doc
 
 image_template = assets2 + "nafha/{}?raw=true"
@@ -37,6 +37,13 @@ items = ImageItems(
     },
 )
 
+parts = {
+    **battery_bus.parts,
+    **pwm_generator.parts,
+    **{
+        "heater-element": "12 V, 4.5 Ω, 32 w",
+    },
+}
 
 docs = [
     design_doc(
