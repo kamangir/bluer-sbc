55ef9a3d2162e2966d2482cbc014d1d6e4cc0adb
kamangir <arash@kamangir.net>
2025-12-05 15:02:57 +0330

    ++ - kamangir/bolt#746

--- bluer_sbc/README/designs/battery_bus/parts.py
+++ bluer_sbc/README/designs/battery_bus/parts.py
@@ -4,7 +4,6 @@ parts = {
     "SLA-Battery": "e.g. 12 V, 7.2 Ah",
     "li-ion-battery": "e.g. 3 x 26650, 5000 mAh 5C, 3.7 V/4.2V",
     "dc-switch": "12V DC 10 A",
-    "dc-power-plug": "",
     "dsn-vc288": "",
     "connector": "1 male",
 }
