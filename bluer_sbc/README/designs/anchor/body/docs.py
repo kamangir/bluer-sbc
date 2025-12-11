cf03e165d4b8970e2c5f2941e30a3bc6197c3335
kamangir <arash@kamangir.net>
2025-12-11 13:08:17 +0330

    start of anchor-and-bettery-bus-li-ion-2025-12-11-13-08-00-pf8btk 🌀 - kamangir/bolt#746

--- bluer_sbc/README/designs/anchor/body/docs.py
+++ bluer_sbc/README/designs/anchor/body/docs.py
@@ -1,21 +1,25 @@
 from bluer_objects.README.items import ImageItems
 
 from bluer_sbc.README.designs.anchor import image_template
-from bluer_sbc.README.designs.anchor.body import v1
+from bluer_sbc.README.designs.anchor.body import v1, v2
 
-docs = [
-    {
-        "path": "../docs/anchor/body",
-        "items": ImageItems(
-            {
-                image_template.format("20251205_175910.jpg"): "",
-                image_template.format("20251205_175916.jpg"): "",
-                image_template.format("20251205_175920.jpg"): "",
-                image_template.format("20251205_175932.jpg"): "",
-                image_template.format("20251205_175936.jpg"): "",
-                image_template.format("20251205_175941.jpg"): "",
-                image_template.format("20251205_175953.jpg"): "",
-            }
-        ),
-    }
-] + v1.docs
+docs = (
+    [
+        {
+            "path": "../docs/anchor/body",
+            "items": ImageItems(
+                {
+                    image_template.format("v3/20251211_121404.jpg"): "",
+                    image_template.format("v3/20251211_121414.jpg"): "",
+                    image_template.format("v3/20251211_121420.jpg"): "",
+                    image_template.format("v3/20251211_121423.jpg"): "",
+                    image_template.format("v3/20251211_121427.jpg"): "",
+                    image_template.format("v3/20251211_121435.jpg"): "",
+                    image_template.format("v3/20251211_121452.jpg"): "",
+                }
+            ),
+        }
+    ]
+    + v1.docs
+    + v2.docs
+)
--- bluer_sbc/README/designs/battery_bus/body/li_ion.py
+++ bluer_sbc/README/designs/battery_bus/body/li_ion.py
@@ -7,6 +7,20 @@ docs = [
         "path": "../docs/battery_bus/body/li-ion.md",
         "items": ImageItems(
             {
+                image_template.format("li-ion/v2/20251211_121307.jpg"): "",
+                image_template.format("li-ion/v2/20251211_121319.jpg"): "",
+                image_template.format("li-ion/v2/20251211_121331.jpg"): "",
+                image_template.format("li-ion/v2/20251211_121339.jpg"): "",
+                image_template.format("li-ion/v2/20251211_121344.jpg"): "",
+                image_template.format("li-ion/v2/20251211_121349.jpg"): "",
+                image_template.format("li-ion/v2/20251211_121357.jpg"): "",
+            }
+        ),
+    },
+    {
+        "path": "../docs/battery_bus/body/li-ion-v1.md",
+        "items": ImageItems(
+            {
                 image_template.format("li-ion/20251204_143912.jpg"): "",
                 image_template.format("li-ion/20251204_143924.jpg"): "",
                 image_template.format("li-ion/20251204_143931.jpg"): "",
@@ -18,5 +32,5 @@ docs = [
                 image_template.format("20251210_204306.jpg"): "",
             }
         ),
-    }
+    },
 ]
