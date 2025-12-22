196654471f488f12a105c3556b771a161ecbdafe
kamangir <arash@kamangir.net>
2025-12-22 09:42:40 +0330

    start of rangin-2025-12-22-09-42-24-k8x5vs 🌀

--- bluer_sbc/imager/camera/__init__.py
+++ bluer_sbc/imager/camera/__init__.py
@@ -1,6 +1,8 @@
 from bluer_options import host
 
-if host.is_rpi():
+from bluer_sbc import env
+
+if not env.BLUER_SBC_CAMERA_FORCE_GENERIC and host.is_rpi():
     if host.is_64bit():
         from bluer_sbc.imager.camera.rpi_64_bit import RPI_64_bit_Camera as Camera
     else:
