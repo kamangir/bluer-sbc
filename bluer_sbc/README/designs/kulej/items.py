e704eb8725f6b4a71ecee278735c2d6262548e84
kamangir <arash@kamangir.net>
2026-01-18 14:39:09 +0330

    ++

--- bluer_sbc/README/designs/kulej/items.py
+++ bluer_sbc/README/designs/kulej/items.py
@@ -4,13 +4,6 @@ from bluer_sbc.README.designs.kulej import image_template
 
 items = ImageItems(
     {
-        image_template.format("20260118_142641.jpg"): "",
-        image_template.format("20260118_142651.jpg"): "",
-        image_template.format("20260118_142656.jpg"): "",
-        image_template.format("20260118_142701.jpg"): "",
-        image_template.format("20260118_142706.jpg"): "",
-        image_template.format("20260118_142714.jpg"): "",
-        image_template.format("20260118_142724.jpg"): "",
         image_template.format("20260118_142743.jpg"): "",
     }
 )
