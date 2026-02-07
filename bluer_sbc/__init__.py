8a523caaf051815908fdd4ac089f548beb65683f
kamangir <arash@kamangir.net>
2026-02-07 20:29:49 +0330

    wip

--- bluer_sbc/__init__.py
+++ bluer_sbc/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} AI for single board computers and related designs."
 
-VERSION = "9.467.1"
+VERSION = "9.468.1"
 
 REPO_NAME = "bluer-sbc"
 
--- bluer_sbc/help/joystick.py
+++ bluer_sbc/help/joystick.py
@@ -1,14 +1,12 @@
 from typing import List
 
-from bluer_options.terminal import show_usage, xtra
+from bluer_options.terminal import show_usage
 
 
 def help_install(
     tokens: List[str],
     mono: bool,
 ) -> str:
-    options = xtra("dryrun", mono=mono)
-
     return show_usage(
         [
             "@joystick",
