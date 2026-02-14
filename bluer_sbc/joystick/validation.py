ca6f0ae1cb8b0b77b8d1feaf9b7fccdf3124aae8
kamangir <arash@kamangir.net>
2026-02-14 15:31:21 +0330

    wip

--- bluer_sbc/joystick/validation.py
+++ bluer_sbc/joystick/validation.py
@@ -1,3 +1,6 @@
+import pygame
+from pygame import JOYAXISMOTION, JOYBUTTONDOWN, JOYBUTTONUP, QUIT
+
 from blueness import module
 
 from bluer_sbc import NAME
@@ -8,5 +11,49 @@ NAME = module.name(__file__, NAME)
 
 
 def validate() -> bool:
-    logger.info(f"{NAME}.validate")
-    return True
+    logger.info(f"{NAME}.validate...")
+
+    pygame.init()
+
+    joystick_count = pygame.joystick.get_count()
+    if not joystick_count:
+        logger.error("joystick not found.")
+        return False
+    if joystick_count > 1:
+        logger.warning("{} joysticks found.")
+
+    joystick = pygame.joystick.Joystick(0)
+
+    joystick.init()
+
+    # Set up a clock to control the loop speed
+    clock = pygame.time.Clock()
+    running: bool = True
+    success: bool = True
+    while running:
+        try:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == JOYBUTTONDOWN:
+                    print(f"Joystick button {event.button} pressed.")
+                elif event.type == JOYBUTTONUP:
+                    print(f"Joystick button {event.button} released.")
+                elif event.type == JOYAXISMOTION:
+                    # Check which axis was moved (x, y, or others)
+                    axis = event.axis
+                    value = event.value
+                    print(f"Joystick axis {axis} moved to {value}.")
+        except KeyboardInterrupt:
+            running = False
+            logger.warning("Ctrl+C, stopping...")
+        except Exception as e:
+            logger.error(e)
+            running = False
+            success = False
+
+        clock.tick(60)
+
+    pygame.quit()
+
+    return success
