7a91fd13104267ca338eeb431bd67923e785f21d
kamangir <arash@kamangir.net>
2026-02-18 13:57:06 +0330

    wip

--- bluer_sbc/parts/db.py
+++ bluer_sbc/parts/db.py
@@ -406,14 +406,6 @@ db_of_parts["dsn-vc288"] = Part(
     ],
 )
 
-db_of_parts["dc-volt-meter"] = Part(
-    info=[
-        "DC volt meter",
-    ],
-    images=[
-        "dsn-vc288.jpg",
-    ],
-)
 
 db_of_parts["nuts-bolts-spacers"] = Part(
     info=[
